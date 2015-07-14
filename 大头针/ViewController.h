f56b64f5546c0c0368633022bb44ca02d8887b4e
lili <apple@lilili.local>
2015-07-14 20:23:47 +0800

    Initial Commit

---
+++ 大头针/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  大头针
+//
+//  Created by lili on 15/7/14.
+//  Copyright (c) 2015年 lili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 大头针/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  大头针
+//
+//  Created by lili on 15/7/14.
+//  Copyright (c) 2015年 lili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
